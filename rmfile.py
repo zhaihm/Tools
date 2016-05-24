e8694cfdd908e8e37951e71c550d79c645938536
Martin Zhai <hmpro74@hotmail.com>
2016-05-24 21:08:19 +0800

    add parse command line args

--- rmfile.py
+++ rmfile.py
@@ -1,16 +1,43 @@
-# remove file or folder recursively
+# Remove file or folder recursively
 import os
 import shutil
+import sys, getopt
 
-def rmfile(dir_path, filename, recursively):
+def rmfile(dir_path, filename):
     for name in os.listdir(dir_path):
         full_path = os.path.join(dir_path, name)
         if name == filename:
-            if os.path.isdir(full_path):
-                shutil.rmtree(full_path)
-            else:
+            if os.path.isfile(full_path):
                 os.remove(full_path)
+            elif removeDirectory:
+                shutil.rmtree(full_path)
         elif os.path.isdir(full_path):
-            rmfile(full_path, filename, recursively)
+            rmfile(full_path, filename)
 
-rmfile('D:\\a', 'rm', True)
+def usage():
+    print('rmfile.py usage:')
+    print('-r file to remove')
+    print('-d remove matched directory')
+
+dir_path = ""
+filename = ""
+removeDirectory = False
+
+opts, args = getopt.getopt(sys.argv[1:], "r:d")
+
+if len(args) == 0:
+    usage()
+    sys.exit()
+dir_path = args[0]
+
+for op, value in opts:
+    if op == "-r":
+        filename = value
+    elif op == '-d':
+        removeDirectory = True
+    else:
+        usage()
+        sys.exit()
+
+rmfile(dir_path, filename)
+
